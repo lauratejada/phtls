2e6a127f73f9c9372013268ba58ed86a0495d6f1
lauratejada <laura2191@hotmail.com>
2020-05-13 11:47:07 -0500

    dos

---
+++ gracias.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>UPCH - Estomatología - Curso Libre - Los Seis Elementos de la Armonía Orofacial</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Icono -->
+	<link rel="icon" type="image/vnd.microsoft.icon" href="images/favicon.ico">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/global.css">
+
+    <meta name="description" content="UPCH - Estomatología - Los Seis Elementos de la Armonía Orofacial - Curso Libre Online">
+      <meta name="keywords" content="UPCH, Cayetano, Universidad, Universidad Peruana Cayetano Heredia, estomatolog
+      ía, estomatologia, 2020, curso libre, Los Seis Elementos de la Armonía Orofacial, Curso Libre Online">
+    <meta name="author" content="Laura Tejada C.">
+
+
+<!-- Facebook Pixel Code -->
+<!--
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '858404434332484');
+fbq('track', 'PageView');
+</script>
+<noscript>
+	<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=858404434332484&ev=PageView&noscript=1"/>
+</noscript>
+-->
+<!-- End Facebook Pixel Code -->
+
+  </head>
+  <body >
+    <div class="row">
+      <div class="container-fluid">
+
+
+    <div class="cabecera_logos" style="background-color: white;padding: 1rem 1.5rem;">
+        <a class="navbar-brand logo" href="#" style="display: inline;" >
+            <img src="images/logo_upch.png" alt="UPCH" class="logo_blanco" width="150">
+        </a>
+        <a class="logo_estomatologia" href="#">
+            <img src="images/logo_estomatologia.png" alt="UPCH-Estomatologia"  width="120" style="float: right; vertical-align: middle;">
+        </a>
+    </div>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="2000" data-pause="false">
+
+      <!--    <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          </ol>
+      -->
+
+          <div class="carousel-inner">
+            <div class="carousel-item active" >
+              <img class="d-block w-100 fondo" src="images/slide.jpg" alt="">
+
+              <div class="carousel-caption">
+                <!-- <img class="logo_upch" src="images/logo_upch.png">  -->
+                <!--<h1 class="text-right fs-21 col-10 col-sm-6 col-md-5 col-lg-4 col-xl-3 float-right" style="color: #004b9e;"><strong>ESTOMATOLOGÍA - <br>CURSO LIBRE</strong></h1>
+                -->
+              </div>
+            </div>
+          </div>
+          <div class="contenedor">
+
+            <div class="fs-72 titulo text-left d-sm-block">
+              <span style="color: #ffffff;background-color: rgba(0,0,205,0.5);padding-left: 10px;padding-right: 10px;">Curso Libre Online- </span><br>
+              <SPAN style="color: #ffffff;background-color: rgba(0,0,205,0.5);padding-left: 10px;padding-right: 10px;"><STRONG>Los Seis Elementos de la Armonía Orofacial</STRONG></SPAN>
+              <div style="color: #ffffff;background-color: rgba(0,0,205,0.5);padding-left: 10px;"class="fs-21 subtitulo">
+                Sábado: <span style="color: ffffff;">09 de mayo</span><br>
+                De: <span style="color: ffffff;">16:00 a 19:00 Hrs.</span><br>
+                Lugar: <span style="color: ffffff;">vía Zoom</span>
+              </div>
+            </div>
+
+            <div class="formulario" style="position: relative;">
+
+               <div class="mensaje msj text-left" style="color:#495057; padding: 2rem 2rem 2rem 2rem;">
+                  <div class="msj_gracias">Gracias<br>Ud. se encuentra inscrito en el curso libre.</div>
+                  <div class="msj_gracias"><br>Si desea pagar el certificado haga click AQUI <br>
+                    <p><a class="btn btn-success" href="https://tiendaupch.pe/">PAGAR AQUÍ</a></p>
+                  </div>
+                  <div class="fs-15"></b></div>
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+  <!--        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        -->
+        </div>
+     <!--   <div>
+        	<div style="color:#ffffff;background-color: rgba(0,0,205,0.8);">Programa 2020 - Curso Teórico - Libre</div>
+        	<div>Previa inscripción solo por la página web, hasta el domingo 8 de Marzo.<br>
+Profesionales que deseen certificado: S/.72.00 soles que serán cancelados el mismo día del curso.<br>
+Los profesionales inscritos tendrán 5 minutos de tolerancia para el ingreso, pasado este tiempo su vacante será ocupada por otro colega.</div>
+
+        </div>
+-->
+
+
+        <div class="row footer">
+          <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 text-center "  style="background-color: #6f9799;">
+            <img src="http://www.upch.edu.pe/famed/faest/landing/images/1.png" alt="" >
+            <p class="fc-blanco">
+              · Primera Universidad del país<br>
+              en ciencias y salud.<br>
+              · Acreditada internacionalmente,<br>
+              y líder en investigación, un <br>
+              complemento indispensable <br>
+              en la formación de  profesionales.
+            </p>
+          </div>
+          <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 text-center bc-2">
+            <img src="http://www.upch.edu.pe/famed/faest/landing/images/2.png" alt="">
+            <p class="fc-blanco">
+              · Alto nivel de empleabilidad.<br>
+              · Oportunidades laborales.<br>
+              · Reconocimiento indiscutible <br>
+              en el campo laboral, que te <br>
+              dará oportunidades ilimitadas <br>
+              desde que egresas.
+            </p>
+          </div>
+          <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 text-center" style="background-color: #004b9e;">
+            <img src="http://www.upch.edu.pe/famed/faest/landing/images/3.png" alt="">
+            <p class="fc-blanco">
+              · Convenios Nacionales<br>
+              e Internacionales. <br>
+              · Nuestros campus cuentan <br>
+              con más de 100 laboratorios <br>
+              docentes y de investigación <br>
+              del más alto estándar.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBR4TD4"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+
+  </body>
+</html>
